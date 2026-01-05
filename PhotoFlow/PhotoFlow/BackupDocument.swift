80d2bf0b54381abcbf4542f08db974049f35b0ea
zxr20250418-create <zxr20250418@gmail.com>
2026-01-05 23:29:49 +0800

    fix(backup): make BackupDocument compile under strict concurrency

--- PhotoFlow/PhotoFlow/BackupDocument.swift
+++ PhotoFlow/PhotoFlow/BackupDocument.swift
@@ -1,3 +1,4 @@
+import Foundation
 import SwiftUI
 import UniformTypeIdentifiers
 
@@ -5,24 +6,30 @@ extension UTType {
     static let sampleMateBackup = UTType(exportedAs: "com.zxr.samplemate.backup")
 }
 
+final class FileWrapperBox: @unchecked Sendable {
+    let value: FileWrapper
+
+    init(_ value: FileWrapper) {
+        self.value = value
+    }
+}
+
 struct SampleMateBackupDocument: FileDocument {
     static var readableContentTypes: [UTType] { [.sampleMateBackup] }
     static var writableContentTypes: [UTType] { [.sampleMateBackup] }
 
-    var rootFileWrapper: FileWrapper
+    var rootFileWrapper: FileWrapperBox
 
     init(rootFileWrapper: FileWrapper = FileWrapper(directoryWithFileWrappers: [:])) {
-        self.rootFileWrapper = rootFileWrapper
+        self.rootFileWrapper = FileWrapperBox(rootFileWrapper)
     }
 
     init(configuration: ReadConfiguration) throws {
-        guard let wrapper = configuration.file else {
-            throw CocoaError(.fileReadCorruptFile)
-        }
-        rootFileWrapper = wrapper
+        let wrapper = configuration.file
+        rootFileWrapper = FileWrapperBox(wrapper)
     }
 
     func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
-        rootFileWrapper
+        rootFileWrapper.value
     }
 }
